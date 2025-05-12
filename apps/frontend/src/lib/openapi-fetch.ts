4db4e89c50611b083368493d2ce395a4c6be3a3c
Jason <abhishek.shaji@thisisnovos.com>
2025-05-13 00:45:09 +0200

    feat: use .env variable for openapi-fetch

--- apps/frontend/src/lib/openapi-fetch.ts
+++ apps/frontend/src/lib/openapi-fetch.ts
@@ -2,6 +2,6 @@ import createClient from 'openapi-fetch';
 import { paths } from '@/types';
 
 export const client = createClient<paths>({
-  baseUrl: `http://localhost:8000`,
+  baseUrl: process.env.API_BASE_URL,
   credentials: 'include',
 });
